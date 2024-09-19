45f30edfb0942ed1f377877aa4aab6dde88cb823
DavidFernandez <125376069+JoseDaviduc@users.noreply.github.com>
2024-09-18 21:59:35 -0600

    Descripción de los cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { PaquetesComponent } from './paquetes/paquetes.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'historia', component: HistoriaComponent },
+  { path: 'paquetes', component: PaquetesComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { PaquetesComponent } from './paquetes/paquetes.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HistoriaComponent,
+    PaquetesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
