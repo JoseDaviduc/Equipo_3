9f82e6af33a8316555b6ebc007144453109a2da3
Estefanny-Tucuch <7979@itescam.edu.mx>
2024-11-25 12:21:04 -0600

    Cambios login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+
+      this.authService.login(email, password).subscribe({
+        next: (response) => {
+          localStorage.setItem('token', response.token); // Almacena el token en localStorage
+          this.router.navigate(['/dashboard']); // Redirige al dashboard
+        },
+        error: (err) => {
+          alert('Credenciales inválidas');
+        }
+      });
+    }
+  }
+}
