ad17d15de1bb5740a9297adb28548140e88aa4f3
Estefanny-Tucuch <7979@itescam.edu.mx>
2024-11-27 17:02:34 -0600

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AppComponent {
   loginForm: FormGroup;
 
+  
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
